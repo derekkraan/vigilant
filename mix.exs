7a2797a2c849d4654a2a25ad24fd0e1d3d81300d
Derek Kraan <derek.kraan@gmail.com>
2019-04-29 08:34:44 +0200

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Vigilant.MixProject do
   def project do
     [
       app: :vigilant,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
